a5336800dd909e9080f0dfda3cb1317a05cad3c4
Maria Wyganowska <115419159+marywyg@users.noreply.github.com>
2024-03-22 17:37:42 +0100

    Update Main.cpp

--- Zadanie2-email/E-mail/Main.cpp
+++ Zadanie2-email/E-mail/Main.cpp
@@ -2,11 +2,7 @@
 #include <regex>
 
 bool isEmailValid(const std::string& email) {
-
-	std::regex email_regex(
-		R"(^[a-zA-Z0-9._]+@[a-zA-Z.-]+\.[a-zA-Z]{2,3}$)"
-	);
-
+	std::regex email_regex("^[a-zA-Z0-9._]+@[a-zA-Z.-]+\\.[a-zA-Z]{2,3}$");
 	return std::regex_match(email, email_regex);
 }
 int main() {
@@ -14,10 +10,10 @@ int main() {
 	std::cout << "Prosze podac adres e-mail: ";
 	std::cin >> email;
 	if (isEmailValid(email)) {
-		std::cout << "Poprawny adres e-mail." << std::endl;
+		std::cout << "Podano poprawny adres e-mail." << std::endl;
 	}
 	else {
-		std::cout << "Niepoprawny adres e-mail." << std::endl;
+		std::cout << "Podano niepoprawny adres e-mail." << std::endl;
 	}
 	return 0;
 }
